2cee5bca8ef58024ac4ce9df663ac77ca5816540
Obakeng <mosadiobakeng7@gmail.com>
2022-07-08 10:55:02 +0200

    Created request and response objects for city command

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import typer
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = typer.Typer()
 
+appid = os.environ['appid']
+
+
 @app.command("city")
-def get_city():
-    typer.echo("Hello")
+def get_city(city_name: str):
+
+    request = requests.get(
+        "https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={appid}")
+
+    response = request.json()
+
+    typer.echo(f"{request}")
+
 
 if __name__ == "__main__":
     app()
